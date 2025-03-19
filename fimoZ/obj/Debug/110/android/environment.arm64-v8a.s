642dba3773fb6603f893fec72eebe8a63cac1bf0
Snedly <78132014+Snedly@users.noreply.github.com>
2025-03-19 23:22:57 +0700

    final changes 2

--- fimoZ/obj/Debug/110/android/environment.arm64-v8a.s
+++ fimoZ/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"915ca11c-8269-4176-ba56-51a8d6538db0"
+	.asciz	"2d8df103-f258-40af-9a17-2cf3d230390f"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- fimoZ/obj/Release/110/android/environment.arm64-v8a.s
+++ fimoZ/obj/Release/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"ac98a7ff-ee57-499f-832c-93831e047895"
+	.asciz	"6b9c88bf-f267-4900-8562-9a1a78775a1c"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
